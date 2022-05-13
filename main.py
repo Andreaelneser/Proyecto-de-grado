736f7d49964a657341e8688341999cdef8eda71b
Andrea Elneser <andreaelneser@javeriana.edu.co>
2022-05-12 19:23:52 -0500

    COmentario

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ urls = []
 for line in lines:
     without = line.replace("\n", "")
     #time.sleep(60+random.randint(0, 120))
-    urls.append("https://cassis.sirtf.com/atlas/cgi/download.py?aorkey={}&ptg=0&product=yaaar_oe_wavesamp".format(without))
+    #urls.append("https://cassis.sirtf.com/atlas/cgi/download.py?aorkey={}&ptg=0&product=yaaar_oe_wavesamp".format(without))
 
 for x in range(0, 3):
     wget.download(urls[x], 'C:/Users/hp/Downloads/Archivos')

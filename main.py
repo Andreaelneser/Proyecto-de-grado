dae1facb887ebf1042fd14f2b1f2e3f1e46a493a
Andreaelneser <69821359+Andreaelneser@users.noreply.github.com>
2022-05-12 19:42:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ for line in lines:
     #time.sleep(60+random.randint(0, 120))
     #urls.append("https://cassis.sirtf.com/atlas/cgi/download.py?aorkey={}&ptg=0&product=yaaar_oe_wavesamp".format(without))
 
-for x in range(0, 3):
+for x in range(0, len(urls)):
     wget.download(urls[x], 'C:/Users/hp/Downloads/Archivos')
     print(x)

1ffb3af83ddea5e10be15774ed5629bd27ee9dcd
Andrea Elneser <69821359+Andreaelneser@users.noreply.github.com>
2023-08-31 14:16:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from astropy.table import Table
 import numpy as np
 import matplotlib.pyplot as plt
 
-#leer tabla con los IDs que necesitan
-
-#sacar el ID a una lista esto lo peuden hacer en pandas.
-#3cambien con un editor todo lo que diga ! por ¨y usan pandas para leer el cvs.con pandas lo pasan a un dataframe y cargan solo pa primera columna.
-
 
 #for id in list_of_ids
 #num=id
@@ -24,13 +19,3 @@ print(hdu['data'])
 #print(dir(hdu))
 plt.plot(wdata,fdata)
 plt.show()
-
-#miren como se guarda una lista en un pickle y lo guardan ahi
-
-##Preguntar a Rafael por este dato qué es y sobre HDU
-
-#hdu = fits.open(archivo) #default is readonly
-#names=tuple([hdu[0].header['COL*DEF'][x] for x,h in enumerate(hdu[0].header['COL*DEF'])])
-#names+=('flag4',) #append flag4, bug in the header keywords for low-res, flag number 4 COLDEF is missing
-#t=fits.BinTableHDU(data=Table(hdu[0].data, names=names), header=hdu[0].header)
-#print (t)
